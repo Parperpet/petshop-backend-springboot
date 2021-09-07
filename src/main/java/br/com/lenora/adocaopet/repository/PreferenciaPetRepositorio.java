96614d74d2b251ef6b7988c73b214ef9103923cf
Pedro Mello <pedromellogomes@gmail.com>
2021-09-07 17:37:13 -0300

    sistema de match

---
+++ src/main/java/br/com/lenora/adocaopet/repository/PreferenciaPetRepositorio.java
@@ -0,0 +1,17 @@
+package br.com.lenora.adocaopet.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.lenora.adocaopet.model.PreferenciaPet;
+
+@Repository
+public interface PreferenciaPetRepositorio extends JpaRepository<PreferenciaPet, Integer> { 
+  @Query(value = "SELECT * FROM preferencia_pet " +
+                 "WHERE id_usuario = :idUsuario", nativeQuery = true)                                        
+  Optional<PreferenciaPet> findByIdUsuario(@Param("idUsuario") Integer idUsuario);
+}
